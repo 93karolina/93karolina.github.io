e459f79268b934c0e1dbf5ee3a6c80a389f099a3
93karolina <62308919+93karolina@users.noreply.github.com>
2020-05-11 20:06:31 +0200

    zrobione zadanie

--- zadania-domowe/ajax/1-pobierz-dane-programisty/js/script.js
+++ zadania-domowe/ajax/1-pobierz-dane-programisty/js/script.js
@@ -2,4 +2,19 @@
 
 $(function () {
     console.log("Pobierz dane programisty");
-  })
+})
+
+let btn = $("#btn");
+
+
+btn.click(function () {
+
+    fetch("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php")
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+
+            let div = $(`<div id='dane-programisty'></div>`).html(`<br> Imię: ${response.imie},<br> Nazwisko: ${response.nazwisko}, <br> Zawód: ${response.zawod}, <br> Firma: ${response.firma}`)
+            btn.after(div);
+        })
+})
